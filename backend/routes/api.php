ebbfa589412f24990d9c631e0892810620fa73b5
Geesara De Silva <166248845+Geesara2000@users.noreply.github.com>
2025-06-08 22:53:46 +0530

    create api for authenticate  and connect with frontend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/admin/login',   [AuthController::class, 'adminLogin']);
+Route::post('/cashier/login', [AuthController::class, 'cashierLogin']);
+
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
